540318534151aaa3d651e04d9900b4b9b5db9cda
Keyur Parekh <keyurparekh47@gmail.com>
2024-02-23 18:44:49 -0500

    primes service first commit

---
+++ src/main/java/edu/iu/kparekh/primesservice/controller/PrimesController.java
@@ -0,0 +1,23 @@
+package edu.iu.kparekh.primesservice.controller;
+
+import edu.iu.kparekh.primesservice.service.IPrimesService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+public class PrimesController {
+
+    IPrimesService primesService;
+
+    public PrimesController(IPrimesService primesService) {
+        this.primesService = primesService;
+    }
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n) {
+        return primesService.isPrime(n);
+    }
+}
+
+
---
+++ src/main/java/edu/iu/kparekh/primesservice/service/IPrimesService.java
@@ -0,0 +1,5 @@
+package edu.iu.kparekh.primesservice.service;
+
+public interface IPrimesService {
+    boolean isPrime(long n);
+}
